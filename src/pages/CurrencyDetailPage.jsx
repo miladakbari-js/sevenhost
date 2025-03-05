c7ba1c3137a12fd849fddf6e23804aa2e3ebfbf2
Milad-Akbari <milad.akbari.st@gmail.com>
2025-03-05 22:52:31 +0330

    Detail Page

--- src/pages/CurrencyDetailPage.jsx
+++ src/pages/CurrencyDetailPage.jsx
@@ -1,20 +1,41 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import styles from "./CurrencyDetailPage.module.css";
+import { RotatingLines } from "react-loader-spinner";
 
 function CurrencyDetailPage() {
   const params = useParams();
-  const [detail , setDetail] = useState();
+  const [detail, setDetail] = useState();
+  const [isLoading, setIsLoading] = useState(true);
   console.log(detail?.description.en);
+  console.log(detail);
 
   useEffect(() => {
-    fetch(`https://api.coingecko.com/api/v3/coins/${params.name.toLowerCase()}`)
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/${params.name.toLowerCase()}?x_cg_demo_api_key=${
+        import.meta.env.VITE_API_KEY
+      }`
+    )
       .then((res) => res.json())
       .then((json) => setDetail(json));
+      setIsLoading(false);
   }, []);
-  return <div>
-    CurrencyDetailPage
-  <p>{detail?.description.en}</p>
-    </div>;
+ 
+  return (
+    <>
+      {isLoading ? (
+        <h1>is Loading...</h1>
+      ) : (
+        <div className={styles.container}>
+          <h2>{params.name}DetailPage</h2>
+          <div className={styles.detail}>
+            <img src={detail?.image.large} />
+            <p>{detail?.description.en}</p>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default CurrencyDetailPage;
