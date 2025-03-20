bfc1ef63885af6c40318d412e0b8d907d59da092
Milad-Akbari <milad.akbari.st@gmail.com>
2025-03-20 20:46:12 +0330

    check

--- src/pages/Page01.jsx
+++ src/pages/Page01.jsx
@@ -1,21 +1,23 @@
-
 import { useDarkMode } from "../services/darkmode";
-import styles from "./Page01.module.css"
+import styles from "./Page01.module.css";
 
 function Page01() {
-  const {darkMode} = useDarkMode();
+  const { darkMode } = useDarkMode();
   return (
     <div className={styles.container}>
       <div className={!darkMode ? styles.left : styles.dark}>
         <p>Manage your daily tasks better without all the stress.</p>
-        <span>Change the way you manage your tasks with our revolutionary project management technology.</span>
-      <div className={styles.buttons}>
-      <button className={styles.started}>Get Started</button>
-      <button className={styles.demo}>Schedule a Demo</button>
-      </div>
+        <span>
+          Change the way you manage your tasks with our revolutionary project
+          management technology.
+        </span>
+        <div className={styles.buttons}>
+          <button className={styles.started}>Get Started</button>
+          <button className={styles.demo}>Schedule a Demo</button>
+        </div>
       </div>
       <div className={styles.image}>
-        <img  src="./public/cart.png" alt="cart.png" />
+        <img src="./public/cart.png" alt="cart.png" />
       </div>
     </div>
   );
